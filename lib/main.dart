aaf97cf2d0a4c35632fee70d95a9f827d4ac45da
nikashdeka <47028816+nikashdeka@users.noreply.github.com>
2020-10-07 16:01:22 +0800

    A Quiz App
    A quiz app developed in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,9 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +67,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer =
+                    quizBrain.getQuestionAnswer();
+                if (correctAnswer == true) {
+                  print('user got it right');
+                } else {
+                  print('the answer is incorrect');
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
@@ -79,12 +94,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer =
+                    quizBrain.getQuestionAnswer();
+                if (correctAnswer == false) {
+                  print('user got it right');
+                } else {
+                  print('the answer is incorrect');
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  // Properties
+  String questionText;
+  bool questionAnswer;
+
+// Constructor
+  Question(this.questionText, this.questionAnswer);
+}
